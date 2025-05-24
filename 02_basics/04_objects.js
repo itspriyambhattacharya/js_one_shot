4ba2724ebc30d68493d7e7c5848536d09262805b
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-24 11:02:18 +0530

    solving the probelm using JSON.stringify()

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -9,3 +9,11 @@ console.log(`Singleton object: ${user}\nNon-singleton object: ${user2}`);
 console.log("Singleton object: ", user);
 console.log("Singleton  toString(): ", user.toString());
 console.log("Non-singleton object: ", user2);
+
+console.log("\nDisplaying using JSON.stringify()\n");
+
+console.log(
+  `Singleton object: ${JSON.stringify(
+    user
+  )}\nNon-singleton object: ${JSON.stringify(user2)}`
+);
