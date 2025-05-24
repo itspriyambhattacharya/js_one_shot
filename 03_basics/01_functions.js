71a61dbac264c3e2105ee72a43783a83a06bc57a
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-24 18:54:11 +0530

    Rest Operator Basics

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -23,3 +23,12 @@ const str = loginUser("Priyam Bhattacharya");
 console.log(str);
 
 console.log(loginUser("Priyam"));
+
+// Rest Operator and function
+
+function tryRest(v1, v2, ...val) {
+  return val;
+}
+
+const arr = tryRest(10, 20, 45, 32, 67);
+console.log(arr);
