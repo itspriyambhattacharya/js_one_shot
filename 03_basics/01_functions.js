8279c5d23e19fae9228a76f61ce84a2a5f08146a
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-24 14:12:10 +0530

    loginUser() function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,11 +1,25 @@
 function greet() {
   console.log("Hello Wolrd");
 }
-greet();
 
 function add2num(a, b) {
   return a + b;
 }
 
+function loginUser(username = "Invalid") {
+  if (username === undefined) {
+    console.log("\nEnter a valid username");
+    return;
+  }
+  return `${username} just logged in.`;
+}
+
+greet();
+
 const result = add2num(4, 6);
 console.log(result);
+
+const str = loginUser("Priyam Bhattacharya");
+console.log(str);
+
+console.log(loginUser("Priyam"));
