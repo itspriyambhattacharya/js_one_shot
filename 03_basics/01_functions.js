1b4eca5fcac9ce7991285c18e84b5641c733290f
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-24 14:00:15 +0530

    Add two numbers using function.

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,7 +1,11 @@
 function greet() {
   console.log("Hello Wolrd");
 }
+greet();
 
-greet();
-greet();
-greet();
+function add2num(a, b) {
+  return a + b;
+}
+
+const result = add2num(4, 6);
+console.log(result);
