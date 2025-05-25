b6200c966b63f7f5a26364d6e711e423a24106d6
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-25 22:47:07 +0530

    Add filter examples for book objects to demonstrate filtering by genre and publication year

--- 05_iterations/06_filter.js
+++ 05_iterations/06_filter.js
@@ -7,3 +7,45 @@ const res2 = arr.filter((val) => {
   return val < 4; // explicit return
 });
 console.log(res2);
+
+// Exercise
+
+const books = [
+  {
+    title: "Book1",
+    genre: "History",
+    publish: 1998,
+    edition: 2005,
+  },
+  {
+    title: "Book2",
+    genre: "Science",
+    publish: 1995,
+    edition: 2005,
+  },
+  {
+    title: "Book3",
+    genre: "Science",
+    publish: 2005,
+    edition: 2005,
+  },
+  {
+    title: "Book4",
+    genre: "Science",
+    publish: 1900,
+    edition: 2005,
+  },
+];
+
+const historyBooks = books.filter((bk) => bk.genre === "History");
+console.log(historyBooks);
+
+const after1995 = books.filter((bk) => bk.publish >= 1995);
+console.log(after1995);
+
+const a1995AndHistory = books.filter((bk) => {
+  if (bk.publish >= 1995 && bk.genre === "Science") {
+    return bk;
+  }
+});
+console.log(a1995AndHistory);
