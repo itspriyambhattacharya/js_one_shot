8a225f3d25ac8dfe4e41b17cada866cddc45fb9b
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-25 11:51:47 +0530

    Add symbol key to user object and checking 'this'

--- 03_basics/03_arrow_functions.js
+++ 03_basics/03_arrow_functions.js
@@ -1,11 +1,17 @@
+const mySymbol = Symbol("123");
 const user = {
   username: "Priyam",
   age: 23,
+  [mySymbol]: "Symbol Key",
   welcome: function () {
     console.log(`Hello ${this.username}, Good Morning`);
+    console.log(this);
   },
 };
 
 user.welcome();
+/*
 user.username = "Sam";
 user.welcome();
+*/
+console.log(this);
