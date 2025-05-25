ed86fbb20455e844ee16ad0ee53932384e26cde7
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-25 23:26:51 +0530

    Add reduce method example with traditional function for array summation

--- 05_iterations/08_reduce.js
+++ 05_iterations/08_reduce.js
@@ -1,1 +1,8 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// using taditional function
+const res1 = arr.reduce(function (acc, val) {
+  console.log(`Accumulator: ${acc}, Current Value: ${val}`);
+  return acc + val;
+}, 0);
+console.log(res1);
