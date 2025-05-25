971a727211badd9236434e845c0702295242090e
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-25 16:01:09 +0530

    Add examples of falsy values and empty data structures in JavaScript

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,1 +1,12 @@
 // Truthy and Falsy values in js
+console.log(`Falsy values are 0, -0, 0n, undefined, null, NaN, false, ""`);
+
+let arr = [];
+let obj = {};
+
+if (arr.length === 0) {
+  console.log("Array is Empty");
+}
+if (Object.keys(obj).length === 0) {
+  console.log("Object is Empty");
+}
