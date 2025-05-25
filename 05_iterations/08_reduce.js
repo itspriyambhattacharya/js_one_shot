c9c76a802022f52d7840882b3867ce294050be2a
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-25 23:31:30 +0530

    Refactor reduce method examples to include implicit and explicit return arrow functions

--- 05_iterations/08_reduce.js
+++ 05_iterations/08_reduce.js
@@ -6,3 +6,18 @@ const res1 = arr.reduce(function (acc, val) {
   return acc + val;
 }, 0);
 console.log(res1);
+
+// using arrow function: Implicit return
+
+const res2 = arr.reduce((acc, val) => acc + val, 0);
+console.log(res2);
+
+// using arrow function: explicit return
+console.log("Arrow function with explicit return");
+
+const res3 = arr.reduce((acc, val) => {
+  console.log(`Accumulator: ${acc}, Current Value: ${val}`);
+
+  return acc + val;
+}, 0);
+console.log(res3);
