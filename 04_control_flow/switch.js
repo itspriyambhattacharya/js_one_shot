aa755adb7a877ae0b551bfc6b11bf3d99945e294
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-25 15:36:09 +0530

    Add switch statement example for month display in control flow

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -8,3 +8,23 @@ case value: code; break;
 default: code; break;
 }
  */
+
+const month = 3;
+
+switch (month) {
+  case 1:
+    console.log("Jan");
+    break;
+  case 2:
+    console.log("Feb");
+    break;
+  case 3:
+    console.log("Mar");
+    break;
+  case 4:
+    console.log("Apr");
+    break;
+  default:
+    console.log("Invalid");
+    break;
+}
