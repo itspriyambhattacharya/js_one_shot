900ff25cab9d91aa0737274ea0079eb97767d712
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-25 21:10:02 +0530

    Add array of user objects for demonstration of forEach usage

--- 05_iterations/05_forEach_loop.js
+++ 05_iterations/05_forEach_loop.js
@@ -16,3 +16,28 @@ language.forEach((val) => {
 language.forEach((val, idx, arr) => {
   console.log(val, idx, language);
 });
+
+// Array of Objects
+
+const users = [
+  {
+    id: 1,
+    name: "Priyam",
+    age: 23,
+  },
+  {
+    id: 1,
+    name: "Rohit",
+    age: 21,
+  },
+  {
+    id: 1,
+    name: "Ronit",
+    age: 20,
+  },
+  {
+    id: 1,
+    name: "Rahul",
+    age: 22,
+  },
+];
