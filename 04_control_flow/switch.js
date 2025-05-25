2674a09a1647b05c3ead92e5f5e564f0ec1e876f
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-25 15:40:45 +0530

    Refactor switch statement example to use let and added code to display default condition.

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -9,7 +9,27 @@ default: code; break;
 }
  */
 
-const month = 3;
+let month = 3;
+
+switch (month) {
+  case 1:
+    console.log("Jan");
+    break;
+  case 2:
+    console.log("Feb");
+    break;
+  case 3:
+    console.log("Mar");
+    break;
+  case 4:
+    console.log("Apr");
+    break;
+  default:
+    console.log("Invalid");
+    break;
+}
+
+month = "3";
 
 switch (month) {
   case 1:
