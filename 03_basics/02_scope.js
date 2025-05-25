3098e954b1dc289cbbc42acb0026dc300510734f
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-25 10:42:06 +0530

    Add nested function demonstration with scope example

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -10,3 +10,17 @@ if (true) {
 console.log(`a is ${a}`);
 console.log(`b is ${b}`);
 console.log(`c is ${c}`);
+
+// Nested function - Function inside a block(if/loop/ or any othe function)
+
+function one() {
+  const uname = "priyam";
+  function two() {
+    const age = 23;
+    console.log(`Username: ${uname}`);
+  }
+  // console.log(`Age: ${age}`);
+  two();
+}
+
+one();
