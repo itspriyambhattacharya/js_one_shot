71b74f5df8d3d20a77fe5b67b556ced961edd760
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-25 19:37:35 +0530

    Iterating over an array using while loop.

--- 05_iterations/02_while_do_loop.js
+++ 05_iterations/02_while_do_loop.js
@@ -3,3 +3,12 @@ while (i <= 10) {
   console.log(i);
   i++;
 }
+
+let arr = [3, 6, 7, 8, 4, 2, 1, 13];
+i = 0;
+console.log("\nThe array is:");
+
+while (i < arr.length) {
+  console.log(arr[i]);
+  i++;
+}
