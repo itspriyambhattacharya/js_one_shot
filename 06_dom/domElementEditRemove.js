9e390fe61253557ed99bd6e1a88f7cadc97add9a
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-26 16:32:11 +0530

    Refactor language replacement logic to edit both 1st and 2nd languages in the list

--- 06_dom/domElementEditRemove.js
+++ 06_dom/domElementEditRemove.js
@@ -21,7 +21,7 @@ addLanguage("Rust");
  * Replace the 2nd language of the list with "Fortran"
  */
 
-console.log("Edit");
+console.log("Edit 2nd child");
 
 const secondLi = document.querySelector(".language>li:nth-child(2)");
 console.log(secondLi);
@@ -33,3 +33,18 @@ newLi.appendChild(newText);
 newLi.textContent = "Fortran";
 console.log(newLi);
 secondLi.replaceWith(newLi);
+
+/**
+ * Replace the 1st language of the list with "Fortran"
+ */
+
+console.log("Edit 1st child");
+const firstLanguage = document.querySelector(".language>li:first-child");
+console.log(firstLanguage);
+
+const tsTxt = document.createTextNode("Typescript");
+const newLiTs = document.createElement("li");
+newLiTs.appendChild(tsTxt);
+console.log(newLiTs);
+
+firstLanguage.replaceWith(newLiTs);
