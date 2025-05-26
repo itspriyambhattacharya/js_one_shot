fc6b9ed8d8fa167a0d3df859c6ea991b78d623dd
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-26 20:01:26 +0530

    Refactor form submission to parse height and weight inputs as numbers

--- 08_project 2/script.js
+++ 08_project 2/script.js
@@ -4,6 +4,6 @@ console.log(form);
 form.addEventListener("submit", (e) => {
   e.preventDefault(); // prevents the default operation of sending the values to server
 
-  const ht = document.querySelector("#height").value;
-  console.log(ht);
+  let ht = Number(document.querySelector("#height").value);
+  let wt = Number(document.querySelector("#weight").value);
 });
