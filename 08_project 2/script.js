63580334fa46f6756621fb414376eea78d84098f
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-26 20:26:56 +0530

    Add validation for height and weight inputs in BMI Calculator

--- 08_project 2/script.js
+++ 08_project 2/script.js
@@ -6,4 +6,22 @@ form.addEventListener("submit", (e) => {
 
   let ht = Number(document.querySelector("#height").value);
   let wt = Number(document.querySelector("#weight").value);
+  let res = document.querySelector("#results");
+  console.log(ht);
+
+  console.log(res);
+  res.innerHTML = "";
+
+  if (ht === "" || ht <= 0 || isNaN(ht)) {
+    const textNode = document.createTextNode("Enter a valid Height\n");
+    res.appendChild(textNode);
+  } else if (wt === "" || wt <= 0 || isNaN(wt)) {
+    const textNode = document.createTextNode("Enter a valid Weight");
+    res.appendChild(textNode);
+  } else {
+    let ans = wt / ((ht * ht) / 10000);
+    ans = ans.toFixed(2);
+    res.innerHTML = `${ans}`;
+    console.log("The result is: ", ans);
+  }
 });
