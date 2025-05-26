73ed23c22a586fbff841ac061046e19b5094ec15
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-26 10:09:20 +0530

    Add text content access methods and explanations for innerText, textContent, and innerHTML

--- 06_dom/script.js
+++ 06_dom/script.js
@@ -8,3 +8,21 @@ title.setAttribute(
 ); // not recommended
 
 title.style.color = "purple"; // recommended
+
+// Accessing the text content of the element
+
+const tc = title.textContent;
+console.log(`The text content of the element is: ${tc}`);
+
+const it = title.innerText;
+
+console.log(it);
+
+/**
+ * Difference between textContent and innerText
+ * textContent shows all the text content of the element, even those that are invisible by display:none or visibility hidden.
+ * innerText only shows that portion of the text that is visible
+ */
+
+const ihtml = title.innerHTML;
+console.log(`The innerHTML is: ${ihtml}`);
