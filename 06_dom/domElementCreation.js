c057743f84a741c74d8c53901d525ccb267125f6
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-26 13:03:23 +0530

    Refactor domElementCreation.js to enhance readability and structure by adding console logs for children elements and their innerHTML.

--- 06_dom/domElementCreation.js
+++ 06_dom/domElementCreation.js
@@ -1,2 +1,15 @@
-const parent = document.querySelector(".parent");
+const parent = document.querySelector(".parent"); //  select the element with class "parent"
 console.log(parent);
+
+const childrenOfParent = parent.children; // returns an HTMLCollection of all the chindren of the parent
+console.log(childrenOfParent);
+
+console.log(
+  `The innerHTML of the first child is: ${childrenOfParent[1].innerHTML}` // displaying the innerHTML of the child present in position 1
+);
+
+console.log("Display using Traditional for loop");
+
+for (let i = 0; i < childrenOfParent.length; i++) {
+  console.log(childrenOfParent[i].innerHTML);
+}
