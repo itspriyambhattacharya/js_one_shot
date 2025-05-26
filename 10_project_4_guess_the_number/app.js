cb37c6ff8677e4acc8047b5c4b7381384b6f138e
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-26 22:47:57 +0530

    Refactor guess evaluation logic to improve readability and maintainability

--- 10_project_4_guess_the_number/app.js
+++ 10_project_4_guess_the_number/app.js
@@ -13,30 +13,34 @@ console.log("Random Number is: ", r);
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  let fl = 1;
   let val = document.getElementById("guessField").value;
   val = Number(val);
   console.log(val, typeof val);
   if (val < 1 || val > 100 || isNaN(val) || val === "") {
     guess.innerHTML = `Invalid number`;
+    fl = 0;
   } else {
     guess.innerHTML = `${val}`;
   }
 
-  if (val < r) {
-    msg.innerHTML = `Low`;
-    chances--;
-    lastResult.innerHTML = `${chances}`;
-  } else if (val > r) {
-    msg.innerHTML = `High`;
-    chances--;
-    lastResult.innerHTML = `${chances}`;
-  } else {
-    msg.innerHTML = `Value Matched`;
-    console.log("Value Matched");
-  }
-  if (chances === 0) {
-    msg.innerHTML = `No chances left, you loose.`;
-    const subBtn = document.getElementById("subt");
-    subBtn.setAttribute("disabled", true);
+  if (fl) {
+    if (val < r) {
+      msg.innerHTML = `Low`;
+      chances--;
+      lastResult.innerHTML = `${chances}`;
+    } else if (val > r) {
+      msg.innerHTML = `High`;
+      chances--;
+      lastResult.innerHTML = `${chances}`;
+    } else {
+      msg.innerHTML = `Value Matched`;
+      console.log("Value Matched");
+    }
+    if (chances === 0) {
+      msg.innerHTML = `No chances left, you loose.`;
+      const subBtn = document.getElementById("subt");
+      subBtn.setAttribute("disabled", true);
+    }
   }
 });
