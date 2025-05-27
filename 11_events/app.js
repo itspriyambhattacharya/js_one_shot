7a7ca78816e67071b12ac4c3a0170f2e93e53a9f
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-27 20:04:49 +0530

    Add initial JavaScript setup for styling and element selection

---
+++ 11_events/app.js
@@ -0,0 +1,8 @@
+const body = document.querySelector("body");
+body.style.backgroundColor = "#212121";
+body.style.color = "#fff";
+body.style.fontFamily = "sans-serif";
+const myUl = document.getElementById("myUl");
+const item = document.querySelector("#item3");
+console.log(myUl);
+console.log(item);
