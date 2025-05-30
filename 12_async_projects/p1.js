406fd604cee1f5a38498d6a124fac919315647bd
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-30 09:45:04 +0530

    Add greeting functionality using setInterval() in p1.js

--- 12_async_projects/p1.js
+++ 12_async_projects/p1.js
@@ -4,3 +4,11 @@ setInterval(() => {
   const time = d.toLocaleTimeString();
   console.log("Time now: " + time);
 }, 1000);
+
+setInterval(
+  (name) => {
+    console.log("Hello: ", name);
+  },
+  2000,
+  "Priyam Bhattacharya"
+);
