0687660a1061e768c02f7b76bcc83432fd8f16bf
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-30 13:51:37 +0530

    Clarify console log messages in promises.js for better understanding of promise execution

--- 13_advanced/promises.js
+++ 13_advanced/promises.js
@@ -1,10 +1,22 @@
+// Promises in Javascript
+// Promise 1
 const promise1 = new Promise((resolve, request) => {
   setTimeout(() => {
-    console.log("Learning promise in javascript");
+    console.log("Learning promise 1 in javascript");
     resolve();
   }, 1000);
 });
 
 promise1.then(() => {
   console.log("Promise 1 Resolved.");
+});
+
+// promise 2
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Learning promise 2 in javascript");
+    resolve();
+  }, 1000);
+}).then(() => {
+  console.log("Promise 2 Resolved");
 });
