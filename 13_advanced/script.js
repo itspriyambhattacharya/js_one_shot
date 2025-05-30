e30121e15d060f550c2e1253af8242f61aa3c820
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-30 11:35:18 +0530

    Implement AJAX request to fetch user data from GitHub API

--- 13_advanced/script.js
+++ 13_advanced/script.js
@@ -3,3 +3,12 @@ const reqUrl = "https://api.github.com/users/itspriyambhattacharya";
 // AJAX
 const xhr = new XMLHttpRequest();
 xhr.open("GET", reqUrl);
+xhr.onreadystatechange = function () {
+  console.log(xhr.readyState);
+
+  if (xhr.readyState === 4) {
+    console.log(this.responseText);
+  }
+}; // mmonitors continuously
+
+xhr.send();
